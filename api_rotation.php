2fef49fddc1e77de9323c37a03eef9e60da54471
FabianLars <FabianLars@vivaldi.net>
2018-10-27 16:54:17 +0200

    finished i guess

--- api_rotation.php
+++ api_rotation.php
@@ -1,12 +1,11 @@
 <?php
-/* config storage. api key will be added to it.
-$configsjson = file_get_contents($_SERVER["DOCUMENT_ROOT"] . '/../httpdocs/configs.json');
-$configs = json_decode($configsjson, true);
-$apiKey = $configs['riotapi']['productionKey'];
-*/
 
-$apiKey = 'RGAPI-ef5b6cf4-3fe3-40e3-92dd-2b3a2b516d1a';
-$requestUrl = 'https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=' . $apiKey;
+//  --- WEEKLY EXECUTION VIA CRON-TASK ---
+
+// 1.) Get Current Champion Rotation from Riot's API
+
+$configs = json_decode(file_get_contents($_SERVER["DOCUMENT_ROOT"] . '/../httpdocs/configs.json'), true);
+$requestUrl = 'https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key='.$configs['riotapi']['productionKey'];
 
 $champList = json_decode(file_get_contents('champsById.json'), true);
 
@@ -20,5 +19,47 @@ foreach ($rotation as $key => $value) {
 
 sort($rotation);
 
-echo "<pre>";
-print_r($rotation);
+// 2.) wikia bot account login; get current page content; regex replace the updated champs; save to wiki
+
+$wApiUrl = 'http://de.leagueoflegends.wikia.com/api.php';
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, $wApiUrl);
+curl_setopt($ch, CURLOPT_COOKIEFILE, '');
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('action' => 'login', 'format' => 'json', 'lgname' => $configs['wikia']['botName'], 'lgpassword' => $configs['wikia']['botPassword'])));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$server_output = curl_exec($ch);
+
+$json = json_decode($server_output);
+
+$token = (string) $json->login->token;
+
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('action' => 'login', 'format' => 'json', 'lgname' => $configs['wikia']['botName'], 'lgpassword' => $configs['wikia']['botPassword'], 'lgtoken' => $token)));
+
+$server_output2 = curl_exec($ch);
+
+curl_setopt($ch, CURLOPT_HTTPGET, 1);
+curl_setopt($ch, CURLOPT_URL, $wApiUrl.'?'.http_build_query(array('action' => 'query', 'format' => 'json', 'prop' => 'revisions', 'rvprop' => 'content', 'titles' => 'Vorlage:Aktuelle_Championrotation')));
+
+$server_output3 = json_decode(curl_exec($ch), true)['query']['pages'];
+
+$page_content = $server_output3[array_keys($server_output3)[0]]['revisions'][0]['*'];
+
+preg_match('/^(\|([A-Za-z\|\s]+))}}/m', $page_content, $matches);
+
+curl_setopt($ch, CURLOPT_URL, $wApiUrl.'?'.http_build_query(array('action' => 'query', 'format' => 'json', 'prop' => 'info', 'intoken' => 'edit', 'titles' => 'Vorlage:Aktuelle_Championrotation')));
+
+$editToken = json_decode(curl_exec($ch), true);
+
+$editToken = reset($editToken['query']['pages'])['edittoken'];
+
+curl_setopt($ch, CURLOPT_URL, $wApiUrl);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('action' => 'edit', 'format' => 'json', 'summary' => 'automated process', 'bot' => 1, 'title' => 'Vorlage:Aktuelle_Championrotation', 'text' => $page_content, 'token' => $editToken)));
+
+curl_exec($ch);
+
+curl_close($ch);
