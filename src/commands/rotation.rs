03e6f0e570988973199cbbe19564cbcc885a5345
FabianLars <fabianlars@fabianlars.de>
2020-02-16 13:47:04 +0100

    Formatted via CLion/Rust-Plugin

--- src/commands/rotation.rs
+++ src/commands/rotation.rs
@@ -1,4 +1,4 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -7,14 +7,14 @@ struct Champ {
     codename: String,
     alias: String,
     id: i32,
-    skins: Vec<Skin>
+    skins: Vec<Skin>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Skin {
     id: i32,
     id_long: i32,
-    name: String
+    name: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -26,43 +26,109 @@ struct Rotations {
 }
 
 pub fn update_rotation() {
-    let riot_api_url = "https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=".to_owned() + &std::env::var("RIOT_API_KEY").unwrap();
+    let riot_api_url = "https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key="
+        .to_owned()
+        + &std::env::var("RIOT_API_KEY").unwrap();
     let wiki_api_url = "https://leagueoflegends.fandom.com/de/api.php";
     let curr_date = chrono::Utc::today();
     let dates = [
-        rename_m((curr_date - chrono::Duration::days(14)).format("%-d. %B %Y").to_string()),
-        rename_m((curr_date - chrono::Duration::days(7)).format("%-d. %B %Y").to_string()),
+        rename_m(
+            (curr_date - chrono::Duration::days(14))
+                .format("%-d. %B %Y")
+                .to_string(),
+        ),
+        rename_m(
+            (curr_date - chrono::Duration::days(7))
+                .format("%-d. %B %Y")
+                .to_string(),
+        ),
         rename_m(curr_date.format("%-d. %B %Y").to_string()),
-        rename_m((curr_date + chrono::Duration::days(7)).format("%-d. %B %Y").to_string())
+        rename_m(
+            (curr_date + chrono::Duration::days(7))
+                .format("%-d. %B %Y")
+                .to_string(),
+        ),
     ];
-    let client = reqwest::blocking::Client::builder().cookie_store(true).build().unwrap();
+    let client = reqwest::blocking::Client::builder()
+        .cookie_store(true)
+        .build()
+        .unwrap();
 
     crate::helpers::wiki::wiki_login(&client);
 
-    let res = client.get("https://fabianlars.de/wapi/champs").send().expect("Can't get champions json file").text().expect("Can't get body from champions json file request");
-    let champions: HashMap<i32, Champ> = serde_json::from_str(&res).expect("Can't convert response to json");
+    let res = client
+        .get("https://fabianlars.de/wapi/champs")
+        .send()
+        .expect("Can't get champions json file")
+        .text()
+        .expect("Can't get body from champions json file request");
+    let champions: HashMap<i32, Champ> =
+        serde_json::from_str(&res).expect("Can't convert response to json");
 
-    let res = client.get(&riot_api_url).send().expect("Can't get rotations").text().expect("Can't get body from rotations request");
+    let res = client
+        .get(&riot_api_url)
+        .send()
+        .expect("Can't get rotations")
+        .text()
+        .expect("Can't get body from rotations request");
     let rotations: Rotations = serde_json::from_str(&res).expect("Can't convert response to json");
 
-    let mut rotation: Vec<String> = rotations.free_champion_ids.iter().map(|x| champions[x].name.clone()).collect();
-    let mut new_players: Vec<String> = rotations.free_champion_ids_for_new_players.iter().map(|x| champions[x].name.clone()).collect();
+    let mut rotation: Vec<String> = rotations
+        .free_champion_ids
+        .iter()
+        .map(|x| champions[x].name.clone())
+        .collect();
+    let mut new_players: Vec<String> = rotations
+        .free_champion_ids_for_new_players
+        .iter()
+        .map(|x| champions[x].name.clone())
+        .collect();
     rotation.sort();
     new_players.sort();
     let new_players: String = new_players.iter().map(|x| "|".to_owned() + x).collect();
 
-    let mut history: Vec<String> = serde_json::from_reader(&std::fs::File::open("history.json").expect("Can't read history.json")).expect("Can't read history.json");
+    let mut history: Vec<String> = serde_json::from_reader(
+        &std::fs::File::open("history.json").expect("Can't read history.json"),
+    )
+    .expect("Can't read history.json");
     history.pop();
     history.insert(0, rotation.iter().map(|x| "|".to_owned() + x).collect());
 
-    serde_json::to_writer(&std::fs::File::create("history.json").expect("Can't write history.json"), &history).expect("Can't write history.json");
+    serde_json::to_writer(
+        &std::fs::File::create("history.json").expect("Can't write history.json"),
+        &history,
+    )
+    .expect("Can't write history.json");
 
-    let res = client.get(reqwest::Url::parse_with_params(wiki_api_url,&[("action", "query"), ("format", "json"), ("prop", "info"), ("intoken", "edit"), ("titles", "Vorlage:Aktuelle_Championrotation")]).unwrap()).send().expect("Can't get edit token").text().expect("Can't get edit token from response body");
+    let res = client
+        .get(
+            reqwest::Url::parse_with_params(
+                wiki_api_url,
+                &[
+                    ("action", "query"),
+                    ("format", "json"),
+                    ("prop", "info"),
+                    ("intoken", "edit"),
+                    ("titles", "Vorlage:Aktuelle_Championrotation"),
+                ],
+            )
+            .unwrap(),
+        )
+        .send()
+        .expect("Can't get edit token")
+        .text()
+        .expect("Can't get edit token from response body");
     let json: serde_json::Value = serde_json::from_str(&res).unwrap();
-    let (_i, o) = json["query"]["pages"].as_object().unwrap().into_iter().next().unwrap();
+    let (_i, o) = json["query"]["pages"]
+        .as_object()
+        .unwrap()
+        .into_iter()
+        .next()
+        .unwrap();
     let edit_token = String::from(o["edittoken"].as_str().unwrap());
 
-    let template = format!(r#"
+    let template = format!(
+        r#"
 <div style="text-align:center; font-size: 125%; font-weight:bold; margin: 2px 0 0;">[[Kostenlose Championrotation]]</div><div style="text-align:left; font-size: 80%; font-weight:bold; margin: 2px 0 0;">[[Vorlage:Aktuelle Championrotation|Bearbeiten]]</div>
 <tabber>Klassisch=
 {{{{#ifeq:{{{{FULLPAGENAME}}}}|Vorlage:Aktuelle Championrotation|{{{{#ifeq:{{{{#time:N|{{{{CURRENTTIMESTAMP}}}}}}}}|2|{{{{#ifexpr:{{{{#expr:{{{{#time:U|{{{{REVISIONTIMESTAMP}}}}}}}}+100000}}}}<{{{{#time:U|{{{{CURRENTTIMESTAMP}}}}}}}}|[[Kategorie:Datumskategorie Championrotation]]}}}}}}}}}}}}{{{{Aktuelle Championrotation/var
@@ -116,11 +182,33 @@ pub fn update_rotation() {
 |lastchecked      = {}
 {}}}}}
 </tabber><noinclude>{{Dokumentation}}<noinclude>
-    "#, history[0], dates[0], dates[1], history[2], dates[1], dates[2], history[1], dates[2], dates[3], history[0], dates[2], new_players);
+    "#,
+        history[0],
+        dates[0],
+        dates[1],
+        history[2],
+        dates[1],
+        dates[2],
+        history[1],
+        dates[2],
+        dates[3],
+        history[0],
+        dates[2],
+        new_players
+    );
 
-    client.post(wiki_api_url)
-        .form(&[("action", "edit"), ("reason", "automated action"), ("bot", "1"), ("title", "Vorlage:Aktuelle_Championrotation"), ("text", &template), ("token", &edit_token)])
-        .send().expect("Can't edit Vorlage:Aktuelle_Championrotation");
+    client
+        .post(wiki_api_url)
+        .form(&[
+            ("action", "edit"),
+            ("reason", "automated action"),
+            ("bot", "1"),
+            ("title", "Vorlage:Aktuelle_Championrotation"),
+            ("text", &template),
+            ("token", &edit_token),
+        ])
+        .send()
+        .expect("Can't edit Vorlage:Aktuelle_Championrotation");
 }
 
 fn rename_m(d: String) -> String {
--- src/main.rs
+++ src/main.rs
@@ -7,25 +7,27 @@ mod helpers;
 enum Cli {
     Delete {
         #[structopt(parse(from_os_str))]
-        path: std::path::PathBuf
+        path: std::path::PathBuf,
     },
     Rotation,
     List {
         list_type: String,
         #[structopt(parse(from_os_str))]
-        destination: std::path::PathBuf
-    }
+        destination: std::path::PathBuf,
+    },
 }
 
 fn main() {
     let args = Cli::from_args();
 
     match args {
-        Cli::Delete { path } => commands::delete::delete_pages(std::fs::read_to_string(&path).expect("could not read file path")),
+        Cli::Delete { path } => commands::delete::delete_pages(
+            std::fs::read_to_string(&path).expect("could not read file path"),
+        ),
         Cli::Rotation => commands::rotation::update_rotation(),
-        Cli::List { list_type, destination } => commands::list::distributor(list_type, destination),
+        Cli::List {
+            list_type,
+            destination,
+        } => commands::list::distributor(list_type, destination),
     }
-
 }
-
-
