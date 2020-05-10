e2eef2851fb730591167cb4b1f56b170b6808fd0
FabianLars <fabianlars@fabianlars.de>
2020-05-10 22:37:29 +0200

    removed option wrap from subcommand

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -92,7 +92,7 @@ enum ListType {
 #[derive(Clap, Debug, PartialEq)]
 struct Cli {
     #[clap(subcommand)]
-    command: Option<Subcommand>,
+    command: Subcommand,
 
     #[clap(short, long, env = "FANDOM_BOT_NAME")]
     name: String,
@@ -104,90 +104,85 @@ struct Cli {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
     match cli.command {
-        None => (),
-        Some(x) => match x {
-            Subcommand::Delete { input } => {
-                delete_pages(Config::new(cli.name, cli.password).with_pathbuf(input)).await?
+        Subcommand::Delete { input } => {
+            delete_pages(Config::new(cli.name, cli.password).with_pathbuf(input)).await?
+        }
+        Subcommand::List {
+            list_type,
+            parameter,
+            output,
+        } => {
+            let cfg = Config::new(cli.name, cli.password)
+                .with_parameter(parameter)
+                .with_pathbuf_opt(output);
+            if list_type == ListType::Exturlusage {
+                ::serde_json::to_writer_pretty(
+                    &std::fs::File::create(cfg.path.clone().file_path())?,
+                    &exturlusage(cfg).await?,
+                )?;
+            } else {
+                ::serde_json::to_writer_pretty(
+                    &std::fs::File::create(cfg.path.clone().file_path())?,
+                    &match list_type {
+                        ListType::Allimages => allimages(cfg).await?,
+                        ListType::Allpages => allpages(cfg).await?,
+                        ListType::Alllinks => alllinks(cfg).await?,
+                        ListType::Allcategories => allcategories(cfg).await?,
+                        ListType::Backlinks => backlinks(cfg).await?,
+                        ListType::Categorymembers => categorymembers(cfg).await?,
+                        ListType::Embeddedin => embeddedin(cfg).await?,
+                        ListType::Imageusage => imageusage(cfg).await?,
+                        ListType::Iwbacklinks => iwbacklinks(cfg).await?,
+                        ListType::Langbacklinks => langbacklinks(cfg).await?,
+                        ListType::Search => search(cfg).await?,
+                        ListType::Protectedtitles => protectedtitles(cfg).await?,
+                        ListType::Querypage => querypage(cfg).await?,
+                        ListType::Wkpoppages => wkpoppages(cfg).await?,
+                        ListType::Unconvertedinfoboxes => unconvertedinfoboxes(cfg).await?,
+                        ListType::Allinfoboxes => allinfoboxes(cfg).await?,
+                        _ => vec![String::new()],
+                    },
+                )?;
             }
-            Subcommand::List {
-                list_type,
-                parameter,
-                output,
-            } => {
-                let cfg = Config::new(cli.name, cli.password)
-                    .with_parameter(parameter)
-                    .with_pathbuf_opt(output);
-                if list_type == ListType::Exturlusage {
-                    ::serde_json::to_writer_pretty(
-                        &std::fs::File::create(cfg.path.clone().file_path())?,
-                        &exturlusage(cfg).await?,
-                    )?;
-                } else {
-                    ::serde_json::to_writer_pretty(
-                        &std::fs::File::create(cfg.path.clone().file_path())?,
-                        &match list_type {
-                            ListType::Allimages => allimages(cfg).await?,
-                            ListType::Allpages => allpages(cfg).await?,
-                            ListType::Alllinks => alllinks(cfg).await?,
-                            ListType::Allcategories => allcategories(cfg).await?,
-                            ListType::Backlinks => backlinks(cfg).await?,
-                            ListType::Categorymembers => categorymembers(cfg).await?,
-                            ListType::Embeddedin => embeddedin(cfg).await?,
-                            ListType::Imageusage => imageusage(cfg).await?,
-                            ListType::Iwbacklinks => iwbacklinks(cfg).await?,
-                            ListType::Langbacklinks => langbacklinks(cfg).await?,
-                            ListType::Search => search(cfg).await?,
-                            ListType::Protectedtitles => protectedtitles(cfg).await?,
-                            ListType::Querypage => querypage(cfg).await?,
-                            ListType::Wkpoppages => wkpoppages(cfg).await?,
-                            ListType::Unconvertedinfoboxes => unconvertedinfoboxes(cfg).await?,
-                            ListType::Allinfoboxes => allinfoboxes(cfg).await?,
-                            _ => vec![String::new()],
-                        },
-                    )?;
-                }
+        }
+        Subcommand::Login => login(Config::new(cli.name, cli.password)).await?,
+        Subcommand::Move { input } => {
+            move_pages(Config::new(cli.name, cli.password).with_pathbuf(input)).await?
+        }
+        Subcommand::Upload { input } => {
+            upload(Config::new(cli.name, cli.password).with_pathbuf(input)).await?
+        }
+        #[cfg(not(feature = "league"))]
+        Subcommand::League { .. } => panic!("Did you forget to set the league feature flag?"),
+        #[cfg(feature = "league")]
+        Subcommand::League { league_type, path } => match league_type {
+            LeagueType::Champs | LeagueType::Champions => champs().await?,
+            LeagueType::Discount | LeagueType::Discounts => {
+                discounts(Config::new(cli.name, cli.password).with_pathbuf_opt(path)).await?
             }
-            Subcommand::Login => login(Config::new(cli.name, cli.password)).await?,
-            Subcommand::Move { input } => {
-                move_pages(Config::new(cli.name, cli.password).with_pathbuf(input)).await?
+            LeagueType::Random => {
+                random(Config::new(cli.name, cli.password).with_pathbuf_opt(path)).await?
             }
-            Subcommand::Upload { input } => {
-                upload(Config::new(cli.name, cli.password).with_pathbuf(input)).await?
+            LeagueType::Rotation | LeagueType::Rotations => {
+                #[cfg(not(feature = "riot-api"))]
+                panic!("Did you forget to set the riot-api feature flag?");
+                #[cfg(feature = "riot-api")]
+                rotation(Config::new(cli.name, cli.password).with_pathbuf_opt(path)).await?
             }
-            #[cfg(not(feature = "league"))]
-            Subcommand::League { .. } => panic!("Did you forget to set the league feature flag?"),
-            #[cfg(feature = "league")]
-            Subcommand::League { league_type, path } => match league_type {
-                LeagueType::Champs | LeagueType::Champions => champs().await?,
-                LeagueType::Discount | LeagueType::Discounts => {
-                    discounts(Config::new(cli.name, cli.password).with_pathbuf_opt(path)).await?
-                }
-                LeagueType::Random => {
-                    random(Config::new(cli.name, cli.password).with_pathbuf_opt(path)).await?
-                }
-                LeagueType::Rotation | LeagueType::Rotations => {
-                    #[cfg(not(feature = "riot-api"))]
-                    panic!("Did you forget to set the riot-api feature flag?");
-                    #[cfg(feature = "riot-api")]
-                    rotation(Config::new(cli.name, cli.password).with_pathbuf_opt(path)).await?
-                }
-                LeagueType::Set => {
-                    set(Config::new(cli.name, cli.password).with_pathbuf_opt(path)).await?
-                }
-            },
-            #[cfg(not(feature = "skylords"))]
-            Subcommand::Skylords { .. } => {
-                panic!("Did you forget to set the skylords feature flag?")
+            LeagueType::Set => {
+                set(Config::new(cli.name, cli.password).with_pathbuf_opt(path)).await?
             }
-            #[cfg(feature = "skylords")]
-            Subcommand::Skylords {
-                skylords_type,
-                path,
-            } => match skylords_type {
-                SkylordsType::Carddata => {
-                    carddata(Config::new(cli.name, cli.password).with_pathbuf_opt(path)).await?
-                }
-            },
+        },
+        #[cfg(not(feature = "skylords"))]
+        Subcommand::Skylords { .. } => panic!("Did you forget to set the skylords feature flag?"),
+        #[cfg(feature = "skylords")]
+        Subcommand::Skylords {
+            skylords_type,
+            path,
+        } => match skylords_type {
+            SkylordsType::Carddata => {
+                carddata(Config::new(cli.name, cli.password).with_pathbuf_opt(path)).await?
+            }
         },
     }
     Ok(())
