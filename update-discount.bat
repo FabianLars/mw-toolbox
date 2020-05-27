a436061411f9b9ea869f8c1ed300110632a54fc7
FabianLars <fabianlars@fabianlars.de>
2020-05-27 23:00:41 +0200

    update-discount is now a bat file to use in win

---
+++ update-discount.bat
@@ -0,0 +1,2 @@
+cd cli
+cargo run --features league league discount -p "E:\Spiele\Riot Games\League of Legends\lockfile"
--- update-discount.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cargo run -p cli --features league league discount -p "E:\Spiele\Riot Games\League of Legends\lockfile"
