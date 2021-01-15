8d1d242f05ec5c7a17d759bd10021daad8398496
FabianLars <fabianlars@fabianlars.de>
2021-01-15 12:39:45 +0100

    f5 proof. Closes #30

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Account, Delete, List, Move, Other } from './pages';
 import './App.css';
 
 const App = () => {
-    const [user, setUser] = useState({ isOnline: false, isPersistent: false, username: '', password: '', url: '' });
+    // useRef to make useEffect skip the change from useState
+    const mounted = useRef(false);
+    // Init user state with sessionstorage or default
+    const [user, setUser] = useState(
+        () =>
+            JSON.parse(window.sessionStorage.getItem('userObj')) ?? {
+                isOnline: false,
+                isPersistent: false,
+                username: '',
+                password: '',
+                url: '',
+            }
+    );
+
+    // Update sessionStorage on every user object change
+    useEffect(() => {
+        if (mounted.current) {
+            window.sessionStorage.setItem('userObj', JSON.stringify(user));
+        } else {
+            mounted.current = true;
+        }
+    }, [user]);
 
     return (
         <Router>
