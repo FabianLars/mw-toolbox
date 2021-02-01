fcb3999f97cc129aba11cecabdf0bacc2fdc560d
FabianLars <fabianlars@fabianlars.de>
2021-02-01 21:19:39 +0100

    wip

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,18 +1,85 @@
-import { Flex, Grid, GridItem, Textarea } from '@chakra-ui/react';
+import { Button, Flex, FormControl, FormLabel, Grid, GridItem, Textarea, Switch, Checkbox } from '@chakra-ui/react';
+import { useState } from 'react';
 import Header from '../components/Header';
 
+const emptyLinePat = /^\s*[\r\n]/gm;
+
 const Edit = ({ isOnline }) => {
+    const [isRunning, setIsRunning] = useState(false);
+    const [isAuto, setIsAuto] = useState(false);
+    const [pageList, setPageList] = useState('');
+    const [pageContent, setPageContent] = useState('');
+    const [currentPage, setCurrentPage] = useState('Not running!');
+
+    const startStop = () => {
+        if (isRunning) {
+        } else {
+            getNextPage();
+        }
+        setIsRunning((state) => !state);
+    };
+
+    const getNextPage = () => {
+        const pages = pageList
+            .trim()
+            .replace(emptyLinePat, '')
+            .split(/\r?\n/)
+            .map((el) => {
+                return el.trim();
+            });
+        setCurrentPage(pages.shift());
+        setPageList(pages.join('\n'));
+    };
+
     return (
         <Flex direction="column" align="center" p="0 1rem 1rem" h="100vh">
             <Header isOnline={isOnline} /* isDisabled={isLoading} */ />
-            <Grid h="100%" w="100%" templateRows="repeat(2, 1fr)" templateColumns="repeat(4, 1fr)" gap={4} mb={4}>
-                <GridItem rowSpan={2} whiteSpace="nowrap">
-                    <Textarea resize="none" h="100%" placeholder="List of pages to operate on. Seperated by newline." />
+            <Grid h="100%" w="100%" templateRows="repeat(3, 1fr)" templateColumns="repeat(4, 1fr)" gap={4}>
+                <GridItem rowSpan={3} whiteSpace="nowrap">
+                    <Textarea
+                        isDisabled={isRunning}
+                        resize="none"
+                        h="100%"
+                        placeholder="List of pages to operate on. Seperated by newline."
+                        value={pageList}
+                        onChange={(e) => setPageList(e.target.value)}
+                    />
                 </GridItem>
-                <GridItem colSpan={3}>
-                    <Textarea resize="none" h="100%" placeholder="Page contents will be displayed here." />
+                <GridItem colSpan={3} rowSpan={2}>
+                    <Textarea
+                        resize="none"
+                        h="100%"
+                        placeholder="Page contents will be displayed here."
+                        value={pageContent}
+                        onChange={(e) => setPageContent(e.target.value)}
+                    />
                 </GridItem>
-                <GridItem colSpan={3}>Controls</GridItem>
+                <GridItem colSpan={3} display="flex" direction="row" justifyContent="space-between">
+                    Current page: {currentPage}
+                    <Flex direction="column" align="center" justify="space-between" h="100%">
+                        <Button
+                            w="100%"
+                            onClick={startStop}
+                            isDisabled={!isOnline}
+                            title={!isOnline ? 'Please login first!' : 'This might take a while!'}
+                        >
+                            {isRunning ? 'Stop' : 'Start'}
+                        </Button>
+                        <Checkbox
+                            isChecked={isAuto}
+                            onChange={(event) => setIsAuto(event.target.checked)}
+                            isDisabled={true /* TODO */}
+                        >
+                            Auto-Save
+                        </Checkbox>
+                        <Button w="100%" isDisabled={!isRunning}>
+                            Skip
+                        </Button>
+                        <Button w="100%" isDisabled={!isRunning}>
+                            Save
+                        </Button>
+                    </Flex>
+                </GridItem>
             </Grid>
         </Flex>
     );
