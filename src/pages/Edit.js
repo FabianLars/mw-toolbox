5493c7917131a2dd0a3ffa0783feee4c360d9297
FabianLars <fabianlars@fabianlars.de>
2021-02-01 19:55:14 +0100

    adjust columns

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -5,14 +5,14 @@ const Edit = ({ isOnline }) => {
     return (
         <Flex direction="column" align="center" p="0 1rem 1rem" h="100vh">
             <Header isOnline={isOnline} /* isDisabled={isLoading} */ />
-            <Grid h="100%" w="100%" templateRows="repeat(2, 1fr)" templateColumns="repeat(3, 1fr)" gap={4} mb={4}>
+            <Grid h="100%" w="100%" templateRows="repeat(2, 1fr)" templateColumns="repeat(4, 1fr)" gap={4} mb={4}>
                 <GridItem rowSpan={2}>
                     <Textarea resize="none" h="100%" placeholder="List of pages to operate on. Seperated by newline." />
                 </GridItem>
-                <GridItem colSpan={2}>
+                <GridItem colSpan={3}>
                     <Textarea resize="none" h="100%" placeholder="Page contents will be displayed here." />
                 </GridItem>
-                <GridItem colSpan={2}>Controls</GridItem>
+                <GridItem colSpan={3}>Controls</GridItem>
             </Grid>
         </Flex>
     );
