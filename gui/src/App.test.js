21b90e743913d7384037b8a8b84f5f27e51bc62d
FabianLars <fabianlars@fabianlars.de>
2021-03-20 14:18:04 +0100

    fix actions/tests

--- gui/src/App.test.js
+++ gui/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders "Not logged in!"', () => {
     render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
+    const linkElement = screen.getByText(/Not logged in!/i);
     expect(linkElement).toBeInTheDocument();
 });
