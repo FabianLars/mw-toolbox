0fa3aa65945a3e925d669bbda1e872d954332d80
FabianLars <fabianlars@fabianlars.de>
2021-04-14 22:56:21 +0200

    update to release

--- gui/src-tauri/src/cmd.rs
+++ gui/src-tauri/src/cmd.rs
@@ -6,7 +6,7 @@ use tauri::command;
 
 use mw_tools::api;
 
-use crate::{CACHE, CLIENT, FILES_HELPER, INIT, SAVED_STATE, SavedState};
+use crate::{SavedState, CACHE, CLIENT, FILES_HELPER, INIT, SAVED_STATE};
 
 #[derive(Debug, Serialize)]
 pub struct LoginResponse {
@@ -233,96 +233,4 @@ pub async fn upload(text: String) -> Result<(), String> {
     )
     .await
     .map_err(|err| err.to_string())
-}
-
-#[derive(Deserialize)]
-#[serde(tag = "cmd", rename_all = "camelCase")]
-pub enum Cmd {
-    Init {
-        callback: String,
-        error: String,
-    },
-    Login {
-        loginname: String,
-        password: String,
-        wikiurl: String,
-        is_persistent: bool,
-        callback: String,
-        error: String,
-    },
-    Logout {
-        callback: String,
-        error: String,
-    },
-
-    /* Cache (used to be window.sessionstorage) */
-    CacheGet {
-        key: String,
-        callback: String,
-        error: String,
-    },
-    CacheSet {
-        key: String,
-        value: serde_json::Value,
-        callback: String,
-        error: String,
-    },
-
-    /* Page commands */
-    Delete {
-        pages: Vec<String>,
-        callback: String,
-        error: String,
-    },
-    Download {
-        files: Vec<String>,
-        callback: String,
-        error: String,
-    },
-    Edit {
-        title: String,
-        content: String,
-        #[serde(default)]
-        summary: String,
-        callback: String,
-        error: String,
-    },
-    // Get Page content for Edit-tab
-    GetPage {
-        page: String,
-        patterns: Vec<FindReplace>,
-        callback: String,
-        error: String,
-    },
-    List {
-        listtype: String,
-        param: Option<String>,
-        callback: String,
-        error: String,
-    },
-    Move {
-        from: Vec<String>,
-        to: Vec<String>,
-        callback: String,
-        error: String,
-    },
-    Purge {
-        #[serde(default)]
-        is_nulledit: bool,
-        pages: Vec<String>,
-        callback: String,
-        error: String,
-    },
-    // Call File dialog
-    UploadDialog {
-        callback: String,
-        error: String,
-    },
-    // Upload rust-side stored files
-    Upload {
-        #[serde(default)]
-        text: String,
-        callback: String,
-        error: String,
-    },
 }
