46e58e8c0453e6699a94f91100cb5fbf047189b5
FabianLars <fabianlars@fabianlars.de>
2021-05-19 23:58:05 +0200

    spead up file downloader

--- crates/mw-tools/src/api/download.rs
+++ crates/mw-tools/src/api/download.rs
@@ -1,8 +1,13 @@
-use std::{fs::File, io::Write};
+use std::{fs::File, io::Write, path::PathBuf};
 
-use crate::{error::ToolsError, response::download::Imageinfo, WikiClient};
+use futures::StreamExt;
+use regex::Regex;
 
-// TODO: download_multiple() with concurrent downloads. download() for single file
+use crate::{
+    error::ToolsError,
+    response::download::{Imageinfo, Page},
+    WikiClient,
+};
 
 pub async fn download<C: AsRef<WikiClient>, S: AsRef<str>>(
     client: C,
@@ -10,42 +15,76 @@ pub async fn download<C: AsRef<WikiClient>, S: AsRef<str>>(
 ) -> Result<(), ToolsError> {
     let client = client.as_ref();
 
-    let mut path = directories_next::UserDirs::new()
+    let path = directories_next::UserDirs::new()
         .and_then(|p| p.download_dir().map(|p| p.to_path_buf()))
         .expect("Can't find user's download folder!");
 
+    let mut titles = String::new();
+    let mut infos: Vec<crate::response::download::Page> = Vec::new();
+
+    let rgxp = Regex::new(r#"[<>:"/\|?*]+"#).unwrap();
+
     for f in files {
         let f = f.as_ref();
-        let file_info: Imageinfo = client
-            .get_into_json(&[
-                ("action", "query"),
-                ("prop", "imageinfo"),
-                ("iiprop", "url"),
-                ("titles", f),
-            ])
-            .await?;
 
-        if let Some(imageinfo) = file_info.query.pages[0].imageinfo.as_ref() {
-            let file_contents = client
-                .client()
-                .get(&imageinfo[0].url)
-                .send()
-                .await?
-                .bytes()
+        if !titles.is_empty() {
+            titles.push('|');
+        }
+        titles.push_str(f);
+
+        if titles.len() >= 1500 {
+            let mut file_infos: Imageinfo = client
+                .get_into_json(&[
+                    ("action", "query"),
+                    ("prop", "imageinfo"),
+                    ("iiprop", "url"),
+                    ("titles", &titles),
+                ])
                 .await?;
 
-            if let Some(pos) = f.find(':') {
-                path.push(f.split_at(pos + 1).1);
-            } else {
-                path.push(f);
-            }
+            infos.append(&mut file_infos.query.pages);
 
-            let mut file = File::create(&path)?;
-            file.write_all(&file_contents)?;
-
-            path.pop();
+            titles.clear();
         }
     }
 
+    futures::stream::iter(infos)
+        .map(|page| download_and_save(client.client().clone(), path.clone(), rgxp.clone(), page))
+        .buffer_unordered(8)
+        .for_each(|entry| async move {
+            if let Ok(title) = entry {
+                log::info!("successfully saved {} to your downloads folder", title)
+            } else {
+                log::error!("{:?}", entry.unwrap_err())
+            }
+        })
+        .await;
+
     Ok(())
+}
+
+async fn download_and_save(
+    client: reqwest::Client,
+    mut path: PathBuf,
+    regex: Regex,
+    page: Page,
+) -> Result<String, ToolsError> {
+    if let Some(imageinfo) = page.imageinfo {
+        let file_contents = client.get(&imageinfo[0].url).send().await?.bytes().await?;
+
+        let file_name = if let Some(pos) = page.title.find(':') {
+            page.title.split_at(pos + 1).1
+        } else {
+            &page.title
+        };
+
+        path.push(regex.replace_all(file_name, "").as_ref());
+
+        let mut file = File::create(&path)?;
+        file.write_all(&file_contents)?;
+
+        Ok(page.title)
+    } else {
+        Err(ToolsError::InvalidInput(page.title))
+    }
 }
