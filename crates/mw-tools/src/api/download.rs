c9ed69d813b4c7aef79ee249a3fbd1d3ac01e510
FabianLars <fabianlars@fabianlars.de>
2021-05-21 20:53:23 +0200

    improve download function

--- crates/mw-tools/src/api/download.rs
+++ crates/mw-tools/src/api/download.rs
@@ -1,7 +1,8 @@
-use std::{fs::File, io::Write, path::PathBuf};
+use std::path::PathBuf;
 
 use futures::StreamExt;
 use regex::Regex;
+use tokio::{fs::File, io::AsyncWriteExt};
 
 use crate::{
     error::ToolsError,
@@ -49,42 +50,42 @@ pub async fn download<C: AsRef<WikiClient>, S: AsRef<str>>(
     }
 
     futures::stream::iter(infos)
-        .map(|page| download_and_save(client.client().clone(), path.clone(), rgxp.clone(), page))
-        .buffer_unordered(8)
-        .for_each(|entry| async move {
-            if let Ok(title) = entry {
-                log::info!("successfully saved {} to your downloads folder", title)
-            } else {
-                log::error!("{:?}", entry.unwrap_err())
-            }
+        .for_each_concurrent(8, |page| {
+            download_and_save(client.client().clone(), path.clone(), &rgxp, page)
         })
         .await;
 
     Ok(())
 }
 
-async fn download_and_save(
+async fn download_and_save(client: reqwest::Client, path: PathBuf, regex: &Regex, page: Page) {
+    match inner(client, path, regex, &page).await {
+        Ok(_) => log::info!(
+            "successfully saved \"{}\" to your downloads folder.",
+            page.title
+        ),
+        Err(err) => log::error!("couldn't download/save \"{}\". Error: {}", page.title, err),
+    }
+}
+
+async fn inner(
     client: reqwest::Client,
     mut path: PathBuf,
-    regex: Regex,
-    page: Page,
-) -> Result<String, ToolsError> {
-    if let Some(imageinfo) = page.imageinfo {
-        let file_contents = client.get(&imageinfo[0].url).send().await?.bytes().await?;
+    regex: &Regex,
+    page: &Page,
+) -> Result<(), ToolsError> {
+    let imageinfo = page
+        .imageinfo
+        .as_ref()
+        .ok_or_else(|| ToolsError::InvalidInput("invalid wiki response".to_string()))?;
+    let file_contents = client.get(&imageinfo[0].url).send().await?.bytes().await?;
 
-        let file_name = if let Some(pos) = page.title.find(':') {
-            page.title.split_at(pos + 1).1
-        } else {
-            &page.title
-        };
+    let file_name = page.title.splitn(2, ':').last().unwrap_or_default(/*This can't happen*/);
 
-        path.push(regex.replace_all(file_name, "").as_ref());
+    path.push(regex.replace_all(file_name, "").as_ref());
 
-        let mut file = File::create(&path)?;
-        file.write_all(&file_contents)?;
+    let mut file = File::create(&path).await?;
+    file.write_all(&file_contents).await?;
 
-        Ok(page.title)
-    } else {
-        Err(ToolsError::InvalidInput(page.title))
-    }
+    Ok(())
 }
