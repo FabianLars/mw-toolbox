63b44deb4a8dbb7105ea484b7e77b8e04d843874
FabianLars <fabianlars@fabianlars.de>
2021-08-22 00:22:35 +0200

    typo

--- gui/src/helpers/invoke.ts
+++ gui/src/helpers/invoke.ts
@@ -4,7 +4,7 @@ import { invoke } from '@tauri-apps/api/tauri';
  * Get an object from the cache managed by rust.
  */
 const getCache = async <T>(key: string) => {
-    return await invoke<T | null>('get_cache', { key });
+    return await invoke<T | null>('cache_get', { key });
 };
 
 /**
@@ -13,7 +13,7 @@ const getCache = async <T>(key: string) => {
  * Returns a boolean indicating if a value behind the key already existed and therefore got updated.
  */
 const setCache = async (key: string, value: any) => {
-    return await invoke('set_cache', { key, value });
+    return await invoke('cache_set', { key, value });
 };
 
 export { getCache, setCache };
