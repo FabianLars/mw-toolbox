ef4dccbb2230aa4ab4ff823cd7001f7f6f7c7585
FabianLars <fabianlars@fabianlars.de>
2022-03-31 19:03:12 +0200

    mock createRoot function in App.test.tsx

--- gui/src/App.test.tsx
+++ gui/src/App.test.tsx
@@ -1,10 +1,20 @@
 import { render, screen } from '@testing-library/react';
-import { describe, expect, it } from 'vitest';
+import { afterEach, describe, expect, it, vi } from 'vitest';
 import App from './App';
 
 // Placeholder test
 
+vi.mock('react-dom/client', () => {
+    return {
+        createRoot: vi.fn(),
+    };
+});
+
 describe('<App>', () => {
+    afterEach(() => {
+        vi.clearAllMocks();
+    });
+
     it('renders "Profile 1"', async () => {
         render(<App />);
         expect(screen.getByText(/Profile Name/i)).toBeDefined();
